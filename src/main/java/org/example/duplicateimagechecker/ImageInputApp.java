d70e279223fb96040ca8ed7fb00088be95632bfd
Francesco D'Arrigo <Francescoda1234@gmail.com>
2024-03-23 13:21:44 -0400

    Added bulk photo checking

--- src/main/java/org/example/duplicateimagechecker/ImageInputApp.java
+++ src/main/java/org/example/duplicateimagechecker/ImageInputApp.java
@@ -8,14 +8,29 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 import javafx.stage.DirectoryChooser;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+
+import java.awt.*;
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 public class ImageInputApp extends Application {
 
+    private String duplicateDirectoryPath = "./duplicate";
+    private String newDirectoryPath = "./new";
     private Label selectedFileLabel;
     private Label selectedDirectoryLabel;
     private Label searchStatusLabel;
-    private CheckBox searchSubdirectoriesCheckBox; // New CheckBox
+    private Label selectedInputDirectoryLabel;
+    private CheckBox searchSubdirectoriesCheckBox;
+    private ToggleGroup selectionToggleGroup;
+    private RadioButton selectSinglePhotoRadioButton;
+    private RadioButton selectDirectoryRadioButton;
 
     private boolean searchForImage(String selectedPhotoPath, String selectedDirectoryPath, boolean searchSubdirectories) {
         File directory = new File(selectedDirectoryPath);
@@ -39,6 +54,98 @@ public class ImageInputApp extends Application {
         }
         return false; // Image not found
     }
+    // Method to perform directory search in a separate thread
+    private void searchInDirectory(String inputDirPath, String directoryPath, boolean searchSubdirectories) {
+        File directory = new File(inputDirPath);
+        File[] files = directory.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if (file.isFile() && file.getName().endsWith(".jpg")) {
+                    // Compare the selected photo with each file in the directory
+                    boolean imageFound = searchForImage(file.getPath(), directoryPath, searchSubdirectories);
+                    if (imageFound) {
+                        // Add photo to dir called duplicate
+                        copyFile(file, duplicateDirectoryPath);
+                    } else {
+                        // Add photo to dir called new
+                        copyFile(file, newDirectoryPath);
+                    }
+                } else if (searchSubdirectories && file.isDirectory()) {
+                    // If searchSubdirectories is enabled and the current file is a directory, recursively search it
+                    searchInDirectory(file.getAbsolutePath(), directoryPath, true);
+                }
+            }
+            // Display directories after all files have been processed
+            displayDirectories();
+        } else {
+            searchStatusLabel.setText("Input Directory is Empty");
+        }
+    }
+
+    private void copyFile(File sourceFile, String destinationDirectory) {
+        try {
+            Path sourcePath = sourceFile.toPath();
+            Path destinationPath = Paths.get(destinationDirectory, sourceFile.getName());
+            Files.copy(sourcePath, destinationPath, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private void createDirectories() {
+        try {
+            Path duplicateDirectory = Paths.get(duplicateDirectoryPath);
+            Path newDirectory = Paths.get(newDirectoryPath);
+
+            if (Files.exists(duplicateDirectory)) {
+                // Clear the duplicate directory if it exists
+                Files.list(duplicateDirectory).forEach(file -> {
+                    try {
+                        Files.delete(file);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+            } else {
+                // Create the duplicate directory if it doesn't exist
+                Files.createDirectories(duplicateDirectory);
+            }
+
+            if (Files.exists(newDirectory)) {
+                // Clear the new directory if it exists
+                Files.list(newDirectory).forEach(file -> {
+                    try {
+                        Files.delete(file);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+            } else {
+                // Create the new directory if it doesn't exist
+                Files.createDirectories(newDirectory);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private void displayDirectories() {
+        try {
+            //searchStatusLabel.setText("Opening directories...");
+
+            // Open duplicate directory
+            File duplicateDirectory = new File(duplicateDirectoryPath);
+            Desktop.getDesktop().open(duplicateDirectory);
+
+            // Open new directory
+            File newDirectory = new File(newDirectoryPath);
+            Desktop.getDesktop().open(newDirectory);
+
+            //searchStatusLabel.setText("Directories opened successfully.");
+        } catch (IOException e) {
+            //searchStatusLabel.setText("Error opening directories: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
 
     @Override
     public void start(Stage primaryStage) {
@@ -47,7 +154,15 @@ public class ImageInputApp extends Application {
         selectedFileLabel = new Label();
         selectedDirectoryLabel = new Label();
         searchStatusLabel = new Label();
-        searchSubdirectoriesCheckBox = new CheckBox("Search Subdirectories"); // Create CheckBox
+        selectedInputDirectoryLabel = new Label();//for dir input
+        searchSubdirectoriesCheckBox = new CheckBox("Search Subdirectories");
+        selectionToggleGroup = new ToggleGroup();
+        selectSinglePhotoRadioButton = new RadioButton("Select 1 Photo");
+        selectDirectoryRadioButton = new RadioButton("Select Directory of Photos");
+        selectSinglePhotoRadioButton.setToggleGroup(selectionToggleGroup);
+        selectSinglePhotoRadioButton.setSelected(true); // Default selection
+        selectDirectoryRadioButton.setToggleGroup(selectionToggleGroup);
+
 
         Button selectFileButton = new Button("Select JPG File");
         selectFileButton.setOnAction(e -> {
@@ -61,7 +176,21 @@ public class ImageInputApp extends Application {
             }
         });
 
-        Button selectDirectoryButton = new Button("Select Directory");
+        Button selectInputDirectoryButton = new Button("Select Directory of photo to check");//Dir photos to check
+        selectInputDirectoryButton.setOnAction(e -> {
+            DirectoryChooser directoryChooser = new DirectoryChooser();
+            directoryChooser.setTitle("Select Directory Containing Images");
+            File selectedDirectory = directoryChooser.showDialog(primaryStage);
+            if (selectedDirectory != null) {
+                selectedInputDirectoryLabel.setText("Selected Directory: " + selectedDirectory.getAbsolutePath());
+            } else {
+                selectedInputDirectoryLabel.setText("No directory selected.");
+            }
+        });
+        selectInputDirectoryButton.setVisible(false);
+        selectedInputDirectoryLabel.setVisible(false);
+
+        Button selectDirectoryButton = new Button("Select Directory of photos to check against");
         selectDirectoryButton.setOnAction(e -> {
             DirectoryChooser directoryChooser = new DirectoryChooser();
             directoryChooser.setTitle("Select Directory Containing Images");
@@ -73,33 +202,81 @@ public class ImageInputApp extends Application {
             }
         });
 
+        // Add event handlers for radio buttons
+        selectSinglePhotoRadioButton.setOnAction(e -> {
+            selectInputDirectoryButton.setVisible(false);
+            selectInputDirectoryButton.setDisable(true);
+            selectedInputDirectoryLabel.setVisible(false);
+            selectedInputDirectoryLabel.setDisable(true);
+
+            selectFileButton.setVisible(true);
+            selectFileButton.setDisable(false);
+            selectedFileLabel.setVisible(true);
+            selectedFileLabel.setDisable(false);
+        });
+
+        selectDirectoryRadioButton.setOnAction(e -> {
+            selectInputDirectoryButton.setVisible(true);
+            selectInputDirectoryButton.setDisable(false);
+            selectedInputDirectoryLabel.setVisible(true);
+            selectedInputDirectoryLabel.setDisable(false);
+
+            selectFileButton.setVisible(false);
+            selectFileButton.setDisable(true);
+            selectedFileLabel.setVisible(false);
+            selectedFileLabel.setDisable(true);
+        });
+
         Button startSearchButton = new Button("Start Search");
         startSearchButton.setOnAction(e -> {
-            if (selectedFileLabel.getText().isEmpty() || selectedDirectoryLabel.getText().isEmpty()) {
-                // If either the file or directory is not selected, display a message
-                searchStatusLabel.setText("Please select both a file and a directory.");
-            } else {
-                // Both file and directory are selected, initiate the search
-                int startIndex = selectedFileLabel.getText().indexOf(":") + 2; // Start index after the ": "
-                String filePath = selectedFileLabel.getText().substring(startIndex);
-                startIndex = selectedDirectoryLabel.getText().indexOf(":") + 2; // Start index after the ": "
-                String directoryPath = selectedDirectoryLabel.getText().substring(startIndex);
-                boolean searchSubdirectories = searchSubdirectoriesCheckBox.isSelected(); // Get checkbox state
-                boolean imageFound = searchForImage(filePath, directoryPath, searchSubdirectories);
-                if (imageFound) {
-                    searchStatusLabel.setText("Image found.");
+            // Check which radio button is selected
+            if (selectInputDirectoryButton.isVisible()) {
+                // Directory of photos selected
+                if (selectedInputDirectoryLabel.getText().isEmpty() || selectedDirectoryLabel.getText().isEmpty()) {
+                    // If either directory is not selected, display a message
+                    searchStatusLabel.setText("Please select both directories.");
                 } else {
-                    searchStatusLabel.setText("Image not found.");
+                    createDirectories();
+                    // Both directories are selected, initiate the search
+                    String inputDirPath = selectedInputDirectoryLabel.getText().substring(selectedInputDirectoryLabel.getText().indexOf(":") + 2);
+                    String directoryPath = selectedDirectoryLabel.getText().substring(selectedDirectoryLabel.getText().indexOf(":") + 2);
+                    boolean searchSubdirectories = searchSubdirectoriesCheckBox.isSelected(); // Get checkbox state
+
+                    // Perform directory search in a separate thread to keep UI responsive
+                    Thread directorySearchThread = new Thread(() -> {
+                        searchInDirectory(inputDirPath, directoryPath, searchSubdirectories);
+                    });
+                    directorySearchThread.start();
                 }
-                // Clear selected file and directory labels
-                selectedFileLabel.setText("");
-                selectedDirectoryLabel.setText("");
+            } else {
+                // Single photo selected
+                if (selectedFileLabel.getText().isEmpty() || selectedDirectoryLabel.getText().isEmpty()) {
+                    // If either the file or directory is not selected, display a message
+                    searchStatusLabel.setText("Please select both a file and a directory.");
+                } else {
+                    // Both file and directory are selected, initiate the search
+                    String filePath = selectedFileLabel.getText().substring(selectedFileLabel.getText().indexOf(":") + 2);
+                    String directoryPath = selectedDirectoryLabel.getText().substring(selectedDirectoryLabel.getText().indexOf(":") + 2);
+                    boolean searchSubdirectories = searchSubdirectoriesCheckBox.isSelected(); // Get checkbox state
+
+                    // Perform single photo search
+                    boolean imageFound = searchForImage(filePath, directoryPath, searchSubdirectories);
+                    if (imageFound) {
+                        searchStatusLabel.setText("Image found.");
+                    } else {
+                        searchStatusLabel.setText("Image not found.");
+                    }
+                    // Clear selected file and directory labels
+                    selectedFileLabel.setText("");
+                    selectedDirectoryLabel.setText("");
+                }
             }
         });
 
+
         VBox root = new VBox(10);
-        root.getChildren().addAll(selectFileButton, selectedFileLabel, selectDirectoryButton, selectedDirectoryLabel, searchSubdirectoriesCheckBox, searchStatusLabel, startSearchButton);
-        Scene scene = new Scene(root, 400, 250);
+        root.getChildren().addAll(selectSinglePhotoRadioButton, selectDirectoryRadioButton, selectInputDirectoryButton, selectedInputDirectoryLabel, selectFileButton, selectedFileLabel, selectDirectoryButton, selectedDirectoryLabel, searchSubdirectoriesCheckBox, searchStatusLabel, startSearchButton);
+        Scene scene = new Scene(root, 400, 400);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
